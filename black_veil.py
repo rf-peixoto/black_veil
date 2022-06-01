8b383c291013570d5e14d7ee3fa2cb3f714ad77a
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-06-01 13:35:01 -0300

    Update black_veil.py

--- black_veil.py
+++ black_veil.py
@@ -1,5 +1,5 @@
 import sys
-import binascii
+import binascii, subprocess
 from secrets import token_urlsafe
 from string import digits, punctuation
 
@@ -30,10 +30,12 @@ def encrypt(content: str, key: str) -> bytes:
         key_id += 1
     return binascii.hexlify(xored.encode())
 
+# Check for compiling:
+generated_file = False
 # -------------------------------------- #
 #  READ FILE                             #
 # -------------------------------------- #
-print("\n\033[94m[*]\033[00m Reading {0}:\t\t\t".format(sys.argv[1]), end="")
+print("\n\033[94m[*]\033[00m Reading {0}:    ".format(sys.argv[1]), end="")
 try:
     with open(sys.argv[1], "r") as fl:
         data = fl.read().split("#-----#")
@@ -47,7 +49,7 @@ except Exception as error:
 #  PREPARE CRYPTER STUFF                 #
 # -------------------------------------- #
 # Generate key:
-print("\033[94m[*]\033[00m Generating key:\t\t\t\t", end="")
+print("\033[94m[*]\033[00m Generating key:    ", end="")
 unique_key = token_urlsafe(128)
 for i in "{0}".format(digits + punctuation):
     if i in unique_key:
@@ -55,7 +57,7 @@ for i in "{0}".format(digits + punctuation):
 print("\033[92mDONE\033[00m")
 
 # Generate decrypt function:
-print("\033[94m[*]\033[00m Generating decrypt function:\t\t", end="")
+print("\033[94m[*]\033[00m Generating decrypt function:    ", end="")
 decrypt_function = """def decrypt(content: bytes, key='{0}') -> str:
     key_id = 0
     xored = ''
@@ -69,7 +71,7 @@ print("\033[92mDONE\033[00m")
 # -------------------------------------- #
 #  PREPARE HEADER                        #
 # -------------------------------------- #
-print("\033[94m[*]\033[00m Preparing header:\t\t\t\t", end="")
+print("\033[94m[*]\033[00m Preparing header:    ", end="")
 header = "import binascii;" + data[0].replace("\n", ";")
 header += "\n{0}".format(decrypt_function)
 print("\033[92mDONE\033[00m")
@@ -77,7 +79,7 @@ print("\033[92mDONE\033[00m")
 # -------------------------------------- #
 #  ENCRYPTING PAYLOAD                    #
 # -------------------------------------- #
-print("\033[94m[*]\033[00m Encrypting payload:\t\t\t\t", end="")
+print("\033[94m[*]\033[00m Encrypting payload:    ", end="")
 try:
     payload = data[1] #.replace("\n", ";")
     encoded_payload = encrypt(payload, unique_key)
@@ -90,22 +92,51 @@ except Exception as error:
 # -------------------------------------- #
 #  CREATE FOOTER                         #
 # -------------------------------------- #
-print("\033[94m[*]\033[00m Generating file footer:\t\t\t", end="")
+print("\033[94m[*]\033[00m Generating file footer:    ", end="")
 footer = 'exec(decrypt({0}))'.format(encoded_payload)
 print("\033[92mDONE\033[00m")
 
 # -------------------------------------- #
 #  CREATE NEW FILE                       #
 # -------------------------------------- #
-print("\033[94m[*]\033[00m Exporting file:\t\t\t\t", end="")
+print("\033[94m[*]\033[00m Exporting file:    ", end="")
 try:
     new_filename = "{0}.output.py".format(sys.argv[1])
     with open(new_filename, "w") as fl:
         fl.write(header + "\n")
         fl.write(footer)
+    generated_file = True
     print("\033[92mDONE\033[00m")
     print("\tFile exported to \033[92m{0}\033[00m\n".format(new_filename))
 except Exception as error:
     print("\033[91mERROR\033[00m")
     print("\t" + str(error))
     sys.exit()
+
+# -------------------------------------- #
+#  COMPILE                               #
+# -------------------------------------- #
+if generated_file:
+    print("\033[94m[*]\033[00m Compile?:")
+    opt = input(">>> ").lower()
+    if opt in ["yes", "y"]:
+        print("\033[94m[*]\033[00m Compiling:    ", end="")
+        # Compile:
+        try:
+            cmd = "pyinstaller --distpath . --name {2} --noconfirm --onefile --key {0} --noconsole {1}".format(unique_key, new_filename, sys.argv[1].split(".")[0])
+            subprocess.run(cmd, shell=True)
+            # Clean:
+            #cmd = "mv dist/{0} .".format(new_filename)
+            cmd = "rm -rf build/ {0}.spec".format(sys.argv[1].split(".")[0])
+            subprocess.run(cmd, shell=True)
+            # SUCCESS:
+            print("\033[92mDONE\033[00m")
+        except Exception as error:
+            print("\033[91mERROR\033[00m")
+            print("\t" + str(error))
+            sys.exit()
+
+# -------------------------------------- #
+#  CLOSE BLACK VEIL                      #
+# -------------------------------------- #
+print("\033[94m[*]\033[00m Thank you.")
